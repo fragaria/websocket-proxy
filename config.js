10923b44a6d4b72659044ecd15c26eea5624d996
Robin Gottfried <robin.gottfried@fragaria.cz>
2020-02-25 13:37:53 +0100

    added possibility to merge config-local

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
-const fs = require('fs'),
-      _  = require('lodash');
+const fs   = require('fs'),
+      path = require('path'),
+      _    = require('lodash');
 
 function val(envName, defaultValue) {
     return (process.env[envName]) ? process.env[envName] : defaultValue;
@@ -18,7 +19,7 @@ let CONFIG = {
     logVerbosity: 3,
 }
 
-if (fs.existsSync('config-local.js') || fs.existsSync('config-local/index.js')) {
+if (fs.existsSync(path.join(__dirname, 'config-local.js')) || fs.existsSync(path.join(__dirname, 'config-local/index.js'))) {
     let localConfig = require('./config-local');
     CONFIG = _.merge(CONFIG, localConfig);
 }
