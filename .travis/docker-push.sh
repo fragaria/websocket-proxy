7a7ba9749b9377f90c8692ebd01388956f403cc7
Robin Gottfried <robin.gottfried@fragaria.cz>
2020-04-17 16:29:53 +0200

    travis tag build

--- .travis/docker-push.sh
+++ .travis/docker-push.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t fragaria/websocket-proxy:latest .
+docker tag fragaria/websocket-proxy:latest fragaria/websocket-proxy:$TRAVIS_TAG
 docker push fragaria/websocket-proxy
